10836c453cf96522da986d5a5e33f1af2211c8b6
KevinLo9231 <kevinlo9231@gmail.com>
2023-12-22 11:26:38 +0800

    第四版
    程式的部分沒有變動
    圖片素材替換
    聲音的部分尚未解決

---
+++ Assets/script/BackgroundMusicManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour
+{
+    public static BackgroundMusicManager instance;
+    public AudioSource audioSource;
+    private string volumeKey = "BGMVolume"; // 用于保存音量设置的键值
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 保持在不同场景中
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        LoadVolume(); // 加载音量设置
+    }
+
+    void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey(volumeKey))
+        {
+            float savedVolume = PlayerPrefs.GetFloat(volumeKey);
+            audioSource.volume = savedVolume;
+        }
+        else
+        {
+            audioSource.volume = 0.5f; // 默认音量
+        }
+    }
+
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat(volumeKey, audioSource.volume);
+        PlayerPrefs.Save();
+    }
+
+    public void PlayMusic(AudioClip music)
+    {
+        audioSource.clip = music;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void SetVolume(float newVolume)
+    {
+        audioSource.volume = newVolume;
+        SaveVolume(); // 保存音量设置
+    }
+}
--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -3,9 +3,9 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public AudioSource bgmAudio; // 音樂模塊
-    public Slider volumeslider; // 音量滑輪
+    public  Slider volumeslider; // 音量滑輪
 
-    private static GameManager music; 
+    private static GameManager music; // 固定一個喇叭變數，讓切換場景時不會被重製
 
     private void Awake()
     {
